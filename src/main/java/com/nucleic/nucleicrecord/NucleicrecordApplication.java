17e2ef2a5415c3c8e6848274e35ab1f019cc5f0f
yongfeng.zhang <yongfeng.zhang@newtouch.com>
2022-05-17 23:33:16 +0800

    [ADD]:创建项目

---
+++ src/main/java/com/nucleic/nucleicrecord/NucleicrecordApplication.java
@@ -0,0 +1,13 @@
+package com.nucleic.nucleicrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucleicrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NucleicrecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nucleic/nucleicrecord/NucleicrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nucleic.nucleicrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NucleicrecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
